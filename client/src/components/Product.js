69428b253f6a18125073af4051743fac9c2c351d
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-10-25 22:13:55 +0530

    update

---
+++ client/src/components/Product.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ProductCard } from "./ProductCard";
+
+const Product = () => {
+  return (
+    <div>
+      <center>
+        <h1>Product</h1>
+      </center>
+      <ProductCard />
+    </div>
+  );
+};
+
+export default Product;
---
+++ client/src/components/ProductCard.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./ProductCard.css";
+
+export const ProductCard = () => {
+  return <div className="Product">ProductCard</div>;
+};
