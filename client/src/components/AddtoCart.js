b0d14739cada0a298ad38ab25cfb61a5e95d3964
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-10-27 19:51:20 +0530

    updated

--- client/src/components/AddtoCart.js
+++ client/src/components/AddtoCart.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useContext } from "react";
+import CartContext from "./CartContext";
 
 export const AddtoCart = () => {
+  const { cart, increase, decrease } = useContext(CartContext);
+  function increasef() {
+    increase();
+  }
   return (
     <div>
-      <button>Add to Cart</button>
+      <button onClick={increasef}>Add to Cart</button>
     </div>
   );
 };
