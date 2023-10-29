35fd0f69c54363ee52c5c59488f035c8fded2ec0
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-10-29 22:27:19 +0530

    updated

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,7 +1,18 @@
-import React from "react";
 import "./ProductCard.css";
+import AddtoCart from "./AddtoCart";
+function ProductCard({ product }) {
+  return (
+    <div className="product">
+      <div>{product.title}</div>
+      <div> {product.price.value}</div>
+      <AddtoCart />
+      <marquee wdith="1368" height="20" scrollamount="10" bgcolor="aqua">
+        Hurry up! Limited offer of {product.title} ends soon!
+      </marquee>
+    </div>
+  );
+}
 
-const ProductCard = () => {
-  return <div className="Product">ProductCard</div>;
-};
 export default ProductCard;
+
+// use  {} when u want to write js inside jsx
