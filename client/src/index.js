76c3778cbfd8fc31bbd23435e5205bc68504b4b3
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-11-01 08:16:21 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-import App from "./App";
+import { BrowserRouter, Navigate } from "react-router-dom";
+//import Login from "./components/Login/Login";
+import Navigater from "./Navigate";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Navigater />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
