0531e8c001916d7f21e3676ce409b207f90c76f2
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2023-11-06 23:36:27 +0530

    Update Sidebar.js

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -7,13 +7,7 @@ const Sidebar = () => {
     const closeBtn = document.querySelector("#btn");
     const searchBtn = document.querySelector(".bx-search");
 
-    const menuBtnChange = () => {
-      if (sidebar.classList.contains("open")) {
-        closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
-      } else {
-        closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
-      }
-    };
+    const menuBtnChange = () => {};
 
     const toggleSidebar = () => {
       sidebar.classList.toggle("open");
