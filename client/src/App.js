c34ade157ba404c517ce24822768986e88f68c77
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2024-04-27 23:14:37 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -18,13 +18,7 @@ export default function App() {
   }
 
   return (
-    <>
-      <CartContext.Provider value={{ cart, increase, decrease }}>
-        <div className="App">
-          <Products />
-        </div>
-      </CartContext.Provider>
-    </>
+  
   );
 }
 
